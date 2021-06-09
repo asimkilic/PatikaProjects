7cba418e30693a621a21d4577bb87f4aa41b7abc
ofn2nvu <ofn2nvu@DESKTOP-KJ3Q02H>
2021-06-10 01:49:49 +0300

    init2

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Projeler
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Hello World2");
         }
     }
 }
